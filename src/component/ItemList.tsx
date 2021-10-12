9bccacd18cf072ba62fb985c5556c5ffe6478a19
chogyejin <whrpwls96@naver.com>
2021-10-13 04:51:27 +0900

    Feat: Add detail page by dynamic routes

--- src/component/ItemList.tsx
+++ src/component/ItemList.tsx
@@ -1,6 +1,7 @@
 import { Grid } from 'semantic-ui-react';
 import { IList } from '../../pages';
 import styles from './ItemList.module.css';
+import Link from 'next/link';
 
 //부모 컴포넌트에서 사용한 IList 재사용
 interface Props {
@@ -14,20 +15,22 @@ export default function ItemList({ list }: Props) {
         <Grid.Row>
           {list.map((item) => (
             <Grid.Column key={item.id}>
-              <a>
-                <div className={styles.wrap}>
-                  <img
-                    src={item.image_link}
-                    alt={item.name}
-                    className={styles.img_item}
-                  />
-                  <strong className={styles.tit_item}>{item.name}</strong>
-                  <span className={styles.txt_info}>
-                    {item.category} {item.product_type}
-                  </span>
-                  <strong className={styles.num_price}>${item.price}</strong>
-                </div>
-              </a>
+              <Link href={`/view/${item.id}`}>
+                <a>
+                  <div className={styles.wrap}>
+                    <img
+                      src={item.image_link}
+                      alt={item.name}
+                      className={styles.img_item}
+                    />
+                    <strong className={styles.tit_item}>{item.name}</strong>
+                    <span className={styles.txt_info}>
+                      {item.category} {item.product_type}
+                    </span>
+                    <strong className={styles.num_price}>${item.price}</strong>
+                  </div>
+                </a>
+              </Link>
             </Grid.Column>
           ))}
         </Grid.Row>
