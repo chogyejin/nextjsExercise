217485e5028018427da1bacdccb04b98accb890e
chogyejin <whrpwls96@naver.com>
2021-10-13 04:47:33 +0900

    Feat: Add <Link> tag for home

--- src/component/Top.tsx
+++ src/component/Top.tsx
@@ -1,16 +1,18 @@
 import { Header } from 'semantic-ui-react';
 import Gnb from './Gnb';
-
+import Link from 'next/link';
 export default function Top() {
   return (
     <div>
       <div style={{ display: 'flex', paddingTop: 20 }}>
         <div style={{ flex: '100px 0 0' }}>
-          <img
-            src="/images/hobbang.png"
-            alt="logo"
-            style={{ display: 'block', width: 80 }}
-          />
+          <Link href="/">
+            <img
+              src="/images/hobbang.png"
+              alt="logo"
+              style={{ display: 'block', width: 80 }}
+            />
+          </Link>
         </div>
         <Header as="h1">호빵맨</Header>
       </div>
