033466e3a080e1e27a58986a86aceb8e977a66bf
chogyejin <whrpwls96@naver.com>
2021-10-15 04:10:57 +0900

    Feat: Add Home, About router in Gnb.tsx

--- src/component/Gnb.tsx
+++ src/component/Gnb.tsx
@@ -1,25 +1,34 @@
+import { useRouter } from 'next/dist/client/router';
 import { Menu } from 'semantic-ui-react';
 
 export default function Gnb() {
-  const activeItem = 'home';
-  const activeItem2 = 'messages';
-  const activeItem3 = 'friends';
+  const router = useRouter();
+  console.log(router);
+
+  let activeItem;
+
+  if (router.pathname === '/') {
+    activeItem = 'home';
+  } else if (router.pathname === '/about') {
+    activeItem = 'about';
+  }
+
+  //location.href나 <a> 이용하여 이동 시 페이지가 전부 새로고침됨. -> SPA 장점 사라짐
+  function goLink(e: any, data: any) {
+    if (data.name === 'home') {
+      router.push('/');
+    } else if (data.name === 'about') {
+      router.push('/about');
+    }
+  }
+
   return (
     <Menu inverted>
+      <Menu.Item name="home" active={activeItem === 'home'} onClick={goLink} />
       <Menu.Item
-        name="home"
-        active={activeItem === 'home'}
-        //onClick={this.handleItemClick}
-      />
-      <Menu.Item
-        name="messages"
-        active={activeItem2 === 'messages'}
-        //onClick={this.handleItemClick}
-      />
-      <Menu.Item
-        name="friends"
-        active={activeItem3 === 'friends'}
-        //onClick={this.handleItemClick}
+        name="about"
+        active={activeItem === 'about'}
+        onClick={goLink}
       />
     </Menu>
   );
